657d7304b3d876eeda0a44eb3d5893bc95e668e8
Cody Swain <swain.cody@gmail.com>
2019-04-22 21:53:48 -0700

    Worked on home page explore interests box

--- helpers.js
+++ helpers.js
@@ -1,11 +1,25 @@
 window.onload = function() {
-		$('a[href^="#"]').on('click', function(event) {
-	    var target = $(this.getAttribute('href'));
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').stop().animate({
-	            scrollTop: target.offset().top-30
-	        }, 1000);
-	    }
+
+	// Smooth page scrolling for navbar 
+	$('a[href^="#"]').on('click', function(event) {
+		var target = $(this.getAttribute('href'));
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').stop().animate({
+			    scrollTop: target.offset().top-30
+			}, 1000);
+		}
 	});
+
+	// Main display show up on hover
+	$('.main-displaybox-project-box').hover(
+		// Hover in function
+		function(){
+			$('.displaybox2').css({'opacity': .85});
+		},
+		//Hover out function
+		function (){
+			$('.displaybox2').css('opacity', 0);
+		}
+	);
 }
