768f17b10ebc3654048f1693d258d0bac150f5a7
Cody Swain <swain.cody@gmail.com>
2019-08-20 22:18:47 -0700

    Cleaned up some code

--- helpers.js
+++ helpers.js
@@ -1,25 +1,24 @@
 window.onload = function() {
-
-	// Smooth page scrolling for navbar 
-	$('a[href^="#"]').on('click', function(event) {
-		var target = $(this.getAttribute('href'));
-		if( target.length ) {
-			event.preventDefault();
-			$('html, body').stop().animate({
-			    scrollTop: target.offset().top-30
-			}, 1000);
-		}
-	});
-
-	// Main display show up on hover
-	$('.main-displaybox-project-box').hover(
-		// Hover in function
-		function(){
-			$('.displaybox2').css({'display': 'flex', 'opacity': .85});
-		},
-		//Hover out function
-		function (){
-			$('.displaybox2').css({'display': 'none', 'opacity': 0});
-		}
-	);
+  // Smooth page scrolling for navbar 
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+	scrollTop: target.offset().top-30
+      }, 1000);
+    }
+  });
+  
+  // Main display show up on hover
+  $('.main-displaybox-project-box').hover(
+    // Hover in function
+    function(){
+      $('.displaybox2').css({'display': 'flex', 'opacity': .85});
+    },
+    //Hover out function
+    function (){
+      $('.displaybox2').css({'display': 'none', 'opacity': 0});
+    }
+  );
 }
